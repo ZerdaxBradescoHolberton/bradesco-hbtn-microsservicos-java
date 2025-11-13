1cfa24dfb836e1f6285f73059769d98a219fffab
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-11-13 08:48:34 -0300

    [Projeto bradesco-hbtn-microsservicos-java/0x02/servicos_API_REST] - Implementação 2.0

--- 0x02/servicos_API_REST/src/main/java/com/example/demo/SongController.java
+++ 0x02/servicos_API_REST/src/main/java/com/example/demo/SongController.java
@@ -39,12 +39,14 @@ public class SongController {
 	public Song addSong(@RequestBody Song song) {
 		// TODO
 		songRepository.addSong(song);
+		return song;
 	}
 
 	@PutMapping(path = "/updateSong", consumes = "application/json", produces = "application/json")
 	public Song updadeSong(@RequestBody Song song) {
 		// TODO
 		songRepository.updateSong(song);
+		return song;
 	}
 
 	@DeleteMapping(path = "/removeSong", consumes = "application/json", produces = "application/json")
