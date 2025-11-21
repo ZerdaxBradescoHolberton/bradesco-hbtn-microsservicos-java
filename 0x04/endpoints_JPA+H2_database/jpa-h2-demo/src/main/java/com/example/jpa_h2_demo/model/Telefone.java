32ab04248f35b71a5a73c87422a83920a039096a
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-11-21 19:44:53 -0300

    [Projeto bradesco-hbtn-microsservicos-java/0x04/endpoints_JPA+H2_database] - Implementação 1.0

---
+++ 0x04/endpoints_JPA+H2_database/jpa-h2-demo/src/main/java/com/example/jpa_h2_demo/model/Telefone.java
@@ -0,0 +1,64 @@
+package com.example.jpa_h2_demo.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Telefone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column
+	private String DDD;
+	@Column
+	private String numero;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "cliente_id")
+	@JsonBackReference
+	private Cliente cliente;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDDD() {
+		return DDD;
+	}
+
+	public void setDDD(String dDD) {
+		DDD = dDD;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	
+}
